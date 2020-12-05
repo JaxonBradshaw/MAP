a0210883e73946ae0fa31fc4b7f74549fec1394e
JaxonBradshaw <71052977+JaxonBradshaw@users.noreply.github.com>
2020-12-05 11:00:53 -0700

    models

--- MAP/User/models.py
+++ MAP/User/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+
+class Organization(models.Model):
+    organization_name = models.CharField(max_length=40)
+    city = models.CharField(max_length=20)
+    state_id = models.ForeignKey(State)
+    email = models.EmailField(max_length=40)
+    size = models.CharField(max_length=20)
+    sector = models.CharField(max_length=20)
+
+    def __str__ (self):
+        return self.organization_name
+class Organization_Admin(Person):
+    title = models.CharField(max_length=20)
+    organization_id = models.ForeignKey(Organization)
+
+    def __str__ (self):
+        pass
