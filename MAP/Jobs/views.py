41fe8e4fa567eb8c4dda09cd356fe7a1c1e9d91f
parkerlg <69094049+parkerlg@users.noreply.github.com>
2020-12-07 13:07:24 -0700

    test

--- MAP/Jobs/views.py
+++ MAP/Jobs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Job_Listing
 
 # Create your views here.
 
 def indexPageView(request) :
     return HttpResponse("Hello")
+
+def testView(request) :
+    data = Job_Listing.objects.all()
+    return render()
