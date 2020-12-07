f623c8e4653e5f1507a46ab0e92c0991c2f3d74e
parkerlg <69094049+parkerlg@users.noreply.github.com>
2020-12-07 16:08:40 -0700

    commit

--- MAP/Jobs/forms.py
+++ MAP/Jobs/forms.py
@@ -1,17 +1,26 @@
 from django import forms
 from .models import Job_Listing, State, Organization, Organization_Admin
 
-
-class JobListingForm(forms.form):
+SKILLS = {
+    ('django', 'Django'),
+    ('python', 'Python'),
+    ('html', 'HTML'),
+    ('iphone', 'iPhone'),
+    ('excel', 'Excel')
+}
+class JobListingForm(forms.ModelForm):
     city = forms.CharField(max_length=30)
     state_id = forms.ModelChoiceField(queryset=State.objects.all())
     job_title = forms.CharField(max_length=20)
     org_admin_id = forms.ModelChoiceField(queryset=Organization_Admin.objects.all())
     contracts = forms.CharField(max_length=30)
     description = forms.CharField(max_length=30)
-    skills = forms.CharField(max_length=30)
+    skills = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=SKILLS)
     total_skills = forms.IntegerField()
     skill_value_rating = forms.IntegerField()
-    external_app_link = forms.CharField(max_length=50, null=True, blank=True)
+    external_app_link = forms.CharField(max_length=50)
     organization_id = forms.ModelChoiceField(queryset=Organization.objects.all())
 
+    class Meta:
+        model = Job_Listing
+        fields = ['city', 'state_id', 'job_title', 'org_admin_id', 'contracts', 'description', 'skills', 'total_skills', 'skill_value_rating', 'organization_id']
--- MAP/Jobs/urls.py
+++ MAP/Jobs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import indexPageView, joblistingPageView, joblistingdetailsPageView, editjoblistingPageView
+from .views import indexPageView, joblistingPageView, joblistingdetailsPageView, editjoblistingPageView, registerJobListingView
 
 urlpatterns = [
     path("", indexPageView, name="landing"),
     path("joblisting/", joblistingPageView, name="job_listing"),
     path("joblistingdetails/<int:joblistingID>/", joblistingdetailsPageView, name="job_listing_details"),
-    path("editjoblisting/<int:joblistingID>/", editjoblistingPageView, name="edit_job_listing")   
+    path("editjoblisting/<int:joblistingID>/", editjoblistingPageView, name="edit_job_listing"),
+    path("register/job_listing/", registerJobListingView, name='register_job_listing')
 ]   
