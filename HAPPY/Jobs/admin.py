10496eaee545bb9ecf685f094a531e00f2f6e08c
parkerlg <69094049+parkerlg@users.noreply.github.com>
2020-12-09 21:26:15 -0700

    lkjdf

--- HAPPY/Jobs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Job_Listing, Job_Offer, Job_Application
-
-# Register your models here.
-admin.site.register(Job_Listing)
-admin.site.register(Job_Offer)
-admin.site.register(Job_Application)
--- HAPPY/ml_examples/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AzurewebserviceConfig(AppConfig):
-    name = 'ml_examples'
